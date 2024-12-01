ba9ad0b078acd1a7f9f4f1a85503b8e95399d0b8
coconusz <sabrinamdisa@gmail.com>
2024-12-01 16:37:30 +0700

    Add folder

---
+++ Pertemuan 2/src/rpl2_pert2_51421375/RPL2_Pert2_51421375.java
@@ -0,0 +1,18 @@
+package rpl2_pert2_51421375;
+
+public class RPL2_Pert2_51421375 {
+
+    public static void main(String[] args) {
+        Mahasiswa mahasiswa = new Mahasiswa();
+        
+        mahasiswa.setName("Sabrina Marta Disa");
+        mahasiswa.setNpm("51421375");
+        mahasiswa.setClassNumber("4IA13");
+        mahasiswa.setBirthYear(2003);
+        mahasiswa.setAlamat("Depok");
+        
+        System.out.println(mahasiswa.toString());
+        
+    }
+    
+}
---
+++ Pertemuan 5/src/main/java/me/sabrina/repository/MahasiswaRepository.java
@@ -0,0 +1,10 @@
+package me.sabrina.repository;
+
+import me.sabrina.model.ModelMahasiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MahasiswaRepository extends JpaRepository<ModelMahasiswa, Long>{
+    
+}
