614a9b80cf5a7f417cd5ac694fabe09ab6c0831e
coconusz <sabrinamdisa@gmail.com>
2024-12-01 16:04:22 +0700

    Add Folder Pertemuan 1

---
+++ src/rpl2_pert1/Component.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rpl2_pert1;
+
+/**
+ *
+ * @author sabri
+ */
+public class Component implements Printable {
+    @Override
+    public void print() {
+        System.out.println("Sabrina Marta Disa - 51421375 - 4IA13");
+    }
+}
